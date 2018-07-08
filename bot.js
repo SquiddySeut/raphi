0b48f1a4e436ce745cfa7f7b7702f0e1686b9175
SquiddySeut <40527468+SquiddySeut@users.noreply.github.com>
2018-07-07 19:30:06 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,8 +24,8 @@ client.on('message', message => {
            waifu=0;
     }
     if (message.content==="Start a conversation"){
-        message.reply("how long do you want us to talk?")
-        message.reply("I'll call my friends");
+        //message.reply("how long do you want us to talk?")
+        message.channel.send("I'll call my friends");
         message.channel.send("Satania-sama, how are you?");
         cs=1;
     }    
